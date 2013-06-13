c1a3515dab8dd8cacf1db6d2cb186b099189f245
Mike <mcraig82890@gmail.com>
2013-06-13 00:26:53 -0500

    Update auto_encoder.m

--- unsupervised/auto_encoder.m
+++ unsupervised/auto_encoder.m
@@ -3,16 +3,13 @@
 % data is column sampled.
 function [model,hidden_outs] = auto_encoder(x, hidden_units, p)
 
-rand('seed', 1);
+path('../preprocessing');
 
 % Set the inputs/outputs
 y = x;
 
 % Normalize the input values
-xmean = repmat(mean(x,2), 1, size(x,2));
-xstd = repmat(std(x,[],2), 1, size(x,2));
-xstd(find(xstd == 0)) = 1;
-x = (x - xmean) ./ xstd; % Normalize data
+x = normalize_features(x);
 
 num_inputs = size(x,1);
 num_hidden = hidden_units;
@@ -23,13 +20,11 @@ w2 = double((rand(num_hidden+1, num_outputs) * 2) - 1);
 
 % Initialize learning parameters
 if (nargin < 3)
-    decay_p = 0.95;
-    eps = 1e-7;
-    max_iterations = 20000;
-else
-    decay_p = p.decay_rate;
-    eps = p.eps;
-    max_iterations = p.num_iters;
+    p.decay_rate = 0.05;
+    p.eps = 1e-8;
+    p.num_iters = 20000;
+    p.min_eps = 1e-12;
+	p.min_gradient = 1e-5;
 end
 egsqg_w1 = 0; egsqg_w2 = 0;
 ddx_w1 = 0; ddx_w2 = 0;
@@ -41,7 +36,13 @@ a_func = 'linear';
 % Regularization parameter
 lambda = 0.02;
 
-for i=1:max_iterations
+% Holds the previous error
+prev_errs = Inf(1);
+
+% Holds the original eps value
+orig_eps = p.eps;
+
+for i=1:p.num_iters
     % Forward pass
     hidden = w1' * [x; ones(1,size(x,2))];
     yhat = w2' * [act_func(hidden); ones(1,size(x,2))];
@@ -56,13 +57,15 @@ for i=1:max_iterations
     d_w1 = dvdw1 * ((dydh * dEdy) .* dhdv)' + lambda*w1;
 
     % Update the weights using ADADELTA algorithm
-    egsqg_w1 = (decay_p * egsqg_w1) + (d_w1 .^ 2);
-    egsqg_w2 = (decay_p * egsqg_w2) + (d_w2 .^ 2);
-    ddx_w1 = (sqrt(exsqx_w1 + eps) ./ sqrt(egsqg_w1 + eps)) .* d_w1;
-    ddx_w2 = (sqrt(exsqx_w2 + eps) ./ sqrt(egsqg_w2 + eps)) .* d_w2;
-    exsqx_w1 = (decay_p * exsqx_w1) + (ddx_w1 .^ 2);
-    exsqx_w2 = (decay_p * exsqx_w2) + (ddx_w2 .^ 2);
+    egsqg_w1 = (p.decay_rate * egsqg_w1) + (d_w1 .^ 2);
+    egsqg_w2 = (p.decay_rate * egsqg_w2) + (d_w2 .^ 2);
+    ddx_w1 = (sqrt(exsqx_w1 + p.eps) ./ sqrt(egsqg_w1 + p.eps)) .* d_w1;
+    ddx_w2 = (sqrt(exsqx_w2 + p.eps) ./ sqrt(egsqg_w2 + p.eps)) .* d_w2;
+    exsqx_w1 = (p.decay_rate * exsqx_w1) + (ddx_w1 .^ 2);
+    exsqx_w2 = (p.decay_rate * exsqx_w2) + (ddx_w2 .^ 2);
 
+	ddx_w1 = 0.01*d_w1;
+	ddx_w2 = 0.01*d_w2;
     % Update the weights
     w1 = w1 - ddx_w1;
     w2 = w2 - ddx_w2;
@@ -78,7 +81,17 @@ for i=1:max_iterations
 
     % Compute goodness of fit
     errs = mean(sqrt(mean((y - yhat) .^ 2, 2)));
-    fprintf('RMS: %f Iteration: %d\n', mean(errs), i); fflush(stdout);
+    fprintf('RMS: %f Iteration: %d Eps: %e W1: %e W2: %e\n', mean(errs), i, p.eps, mean(mean(d_w1)), mean(mean(d_w2))); fflush(stdout);
+	if (errs > prev_errs)
+		p.eps = p.eps * 0.99;
+	else
+		p.eps = p.eps * (1 + (100*orig_eps)); end
+	prev_errs = errs;
+	
+	% If our eps has fallen below a threshold, we are done training
+	grad = max([abs(mean(mean(d_w1))); abs(mean(mean(d_w2)))]);
+	if (p.eps < p.min_eps || grad < p.min_gradient)
+		break; end
 end
 
 end
